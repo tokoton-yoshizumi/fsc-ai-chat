2df49311bb9082a1880a40c653e691fc30c4c0e5
tokoton-yoshizumi <info@tokoton-design.com>
2025-08-27 17:16:50 +0900

    change

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from routes import api  # `routes.py` の Blueprint をインポート
 from scheduler import start_scheduler  # スケジューラーをインポート
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(api)  # `routes.py` を Blueprint として登録
 
 # ★★★ スケジューラーをここで開始する ★★★
