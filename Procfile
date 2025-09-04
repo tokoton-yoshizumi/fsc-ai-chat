0fcb8a55be6be6a756cf93ff97c178e96f0aa427
tokoton-yoshizumi <info@tokoton-design.com>
2025-09-04 10:32:49 +0900

    change

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn -w 1 app:app
+worker: python create_database.py
--- app.py
+++ app.py
@@ -1,15 +1,12 @@
 from flask import Flask, render_template
 from flask_cors import CORS
 from routes import api  # `routes.py` の Blueprint をインポート
-from scheduler import start_scheduler  # スケジューラーをインポート
 
 app = Flask(__name__)
 CORS(app)
 
 app.register_blueprint(api)  # `routes.py` を Blueprint として登録
 
-# ★★★ スケジューラーをここで開始する ★★★
-start_scheduler()
 
 @app.route("/")
 def index():
